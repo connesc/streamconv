8a430def4119cd3a4764f3646bd5f77618876325
Cédric Connes <cedric.connes@gmail.com>
2018-01-25 00:05:21 +0100

    Handle errors in exec commands

--- converters/exec.go
+++ converters/exec.go
@@ -2,6 +2,7 @@ package converters
 
 import (
 	"io"
+	"os"
 	"os/exec"
 
 	"github.com/connesc/streamconv"
@@ -12,18 +13,61 @@ type executor struct {
 	args []string
 }
 
-func (c *executor) Convert(src io.Reader) (dst io.Reader, err error) {
-	cmd := exec.Command(c.name, c.args...)
-	cmd.Stdin = src
-	dst, err = cmd.StdoutPipe()
+type blockingReader struct {
+	reader io.Reader
+	done   <-chan error
+}
+
+func (r *blockingReader) checkError() (err error) {
+	select {
+	case err = <-r.done:
+	default:
+	}
+	return
+}
+
+func (r *blockingReader) Read(p []byte) (n int, err error) {
+	err = r.checkError()
 	if err != nil {
 		return
 	}
 
-	err = cmd.Start()
-	return
+	n, err = r.reader.Read(p)
 
-	// TODO: handle exit status and maybe Stderr
+	if err == io.EOF {
+		err = <-r.done
+		if err == nil {
+			err = io.EOF
+		}
+	} else if err == nil {
+		err = r.checkError()
+	}
+
+	return
+}
+
+func (c *executor) Convert(src io.Reader) (dst io.Reader, err error) {
+	cmd := exec.Command(c.name, c.args...)
+	cmd.Stdin = src
+	cmd.Stderr = os.Stderr
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return
+	}
+
+	done := make(chan error)
+	dst = &blockingReader{stdout, done}
+
+	err = cmd.Start()
+	if err != nil {
+		return
+	}
+
+	go func() {
+		done <- cmd.Wait()
+	}()
+
+	return
 }
 
 func NewExecutor(name string, args []string) streamconv.Converter {
