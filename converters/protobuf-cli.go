234e1c031ab2377d3501e2dac0f0964670f243bc
Cédric Connes <cedric.connes@gmail.com>
2018-03-31 12:06:35 +0200

    Add protobuf converters for the text format

--- converters/protobuf-cli.go
+++ converters/protobuf-cli.go
@@ -62,3 +62,115 @@ func NewProtobufFromJSONCLI(name string) (cli streamconv.ConverterCLI) {
 func RegisterProtobufFromJSON(name string) {
 	streamconv.RegisterConverter(name, NewProtobufFromJSONCLI(name))
 }
+
+type protobufToTextCLI struct {
+	name string
+}
+
+func (c *protobufToTextCLI) PrintUsage(output io.Writer) (err error) {
+	_, err = fmt.Fprintln(output, "TODO")
+	return
+}
+
+func (c *protobufToTextCLI) Parse(args []string) (command streamconv.ConverterCommand, err error) {
+	if len(args) != 2 {
+		return nil, fmt.Errorf("invalid number of arguments (expected 2, got %v)", len(args))
+	}
+
+	command = func() (streamconv.Converter, error) {
+		return NewProtobufToText(args[0], args[1])
+	}
+	return
+}
+
+func NewProtobufToTextCLI(name string) (cli streamconv.ConverterCLI) {
+	return &protobufToTextCLI{name}
+}
+
+func RegisterProtobufToText(name string) {
+	streamconv.RegisterConverter(name, NewProtobufToTextCLI(name))
+}
+
+type protobufFromTextCLI struct {
+	name string
+}
+
+func (c *protobufFromTextCLI) PrintUsage(output io.Writer) (err error) {
+	_, err = fmt.Fprintln(output, "TODO")
+	return
+}
+
+func (c *protobufFromTextCLI) Parse(args []string) (command streamconv.ConverterCommand, err error) {
+	if len(args) != 2 {
+		return nil, fmt.Errorf("invalid number of arguments (expected 2, got %v)", len(args))
+	}
+
+	command = func() (streamconv.Converter, error) {
+		return NewProtobufFromText(args[0], args[1])
+	}
+	return
+}
+
+func NewProtobufFromTextCLI(name string) (cli streamconv.ConverterCLI) {
+	return &protobufFromTextCLI{name}
+}
+
+func RegisterProtobufFromText(name string) {
+	streamconv.RegisterConverter(name, NewProtobufFromTextCLI(name))
+}
+
+type protobufJSONToTextCLI struct {
+	name string
+}
+
+func (c *protobufJSONToTextCLI) PrintUsage(output io.Writer) (err error) {
+	_, err = fmt.Fprintln(output, "TODO")
+	return
+}
+
+func (c *protobufJSONToTextCLI) Parse(args []string) (command streamconv.ConverterCommand, err error) {
+	if len(args) != 2 {
+		return nil, fmt.Errorf("invalid number of arguments (expected 2, got %v)", len(args))
+	}
+
+	command = func() (streamconv.Converter, error) {
+		return NewProtobufJSONToText(args[0], args[1])
+	}
+	return
+}
+
+func NewProtobufJSONToTextCLI(name string) (cli streamconv.ConverterCLI) {
+	return &protobufJSONToTextCLI{name}
+}
+
+func RegisterProtobufJSONToText(name string) {
+	streamconv.RegisterConverter(name, NewProtobufJSONToTextCLI(name))
+}
+
+type protobufTextToJSONCLI struct {
+	name string
+}
+
+func (c *protobufTextToJSONCLI) PrintUsage(output io.Writer) (err error) {
+	_, err = fmt.Fprintln(output, "TODO")
+	return
+}
+
+func (c *protobufTextToJSONCLI) Parse(args []string) (command streamconv.ConverterCommand, err error) {
+	if len(args) != 2 {
+		return nil, fmt.Errorf("invalid number of arguments (expected 2, got %v)", len(args))
+	}
+
+	command = func() (streamconv.Converter, error) {
+		return NewProtobufTextToJSON(args[0], args[1])
+	}
+	return
+}
+
+func NewProtobufTextToJSONCLI(name string) (cli streamconv.ConverterCLI) {
+	return &protobufTextToJSONCLI{name}
+}
+
+func RegisterProtobufTextToJSON(name string) {
+	streamconv.RegisterConverter(name, NewProtobufTextToJSONCLI(name))
+}
--- converters/protobuf.go
+++ converters/protobuf.go
@@ -15,6 +15,7 @@ import (
 var parser = protoparse.Parser{}
 var marshaller = jsonpb.Marshaler{}
 var unmarshaller = jsonpb.Unmarshaler{}
+var textMarshaller = proto.TextMarshaler{}
 
 type toJSON struct {
 	message *dynamic.Message
@@ -82,5 +83,154 @@ func NewProtobufFromJSON(protoFile, messageName string) (converter streamconv.Co
 	message := dynamic.NewMessage(descriptor)
 
 	converter = &fromJSON{message, &proto.Buffer{}}
+	return
+}
+
+type toText struct {
+	message *dynamic.Message
+	buffer  *bytes.Buffer
+}
+
+func (c *toText) Convert(src io.Reader) (dst io.Reader, err error) {
+	c.buffer.Reset()
+	_, err = c.buffer.ReadFrom(src)
+	if err != nil {
+		return
+	}
+
+	err = proto.Unmarshal(c.buffer.Bytes(), c.message)
+	if err != nil {
+		return
+	}
+
+	pr, pw := io.Pipe()
+
+	go func() {
+		err = textMarshaller.Marshal(pw, c.message)
+		pw.CloseWithError(err)
+	}()
+
+	return pr, nil
+}
+
+func NewProtobufToText(protoFile, messageName string) (converter streamconv.Converter, err error) {
+	files, err := parser.ParseFiles(protoFile)
+	if err != nil {
+		return
+	}
+
+	descriptor := files[0].FindMessage(messageName)
+	message := dynamic.NewMessage(descriptor)
+
+	converter = &toText{message, &bytes.Buffer{}}
+	return
+}
+
+type fromText struct {
+	message    *dynamic.Message
+	textBuffer *bytes.Buffer
+	buffer     *proto.Buffer
+}
+
+func (c *fromText) Convert(src io.Reader) (dst io.Reader, err error) {
+	c.textBuffer.Reset()
+	_, err = c.textBuffer.ReadFrom(src)
+	if err != nil {
+		return
+	}
+
+	err = proto.UnmarshalText(c.textBuffer.String(), c.message)
+	if err != nil {
+		return
+	}
+
+	c.buffer.Reset()
+	err = c.buffer.Marshal(c.message)
+	return bytes.NewReader(c.buffer.Bytes()), err
+}
+
+func NewProtobufFromText(protoFile, messageName string) (converter streamconv.Converter, err error) {
+	files, err := parser.ParseFiles(protoFile)
+	if err != nil {
+		return
+	}
+
+	descriptor := files[0].FindMessage(messageName)
+	message := dynamic.NewMessage(descriptor)
+
+	converter = &fromText{message, &bytes.Buffer{}, &proto.Buffer{}}
+	return
+}
+
+type jsonToText struct {
+	message *dynamic.Message
+}
+
+func (c *jsonToText) Convert(src io.Reader) (dst io.Reader, err error) {
+	err = unmarshaller.Unmarshal(src, c.message)
+	if err != nil {
+		return
+	}
+
+	pr, pw := io.Pipe()
+
+	go func() {
+		err = textMarshaller.Marshal(pw, c.message)
+		pw.CloseWithError(err)
+	}()
+
+	return pr, nil
+}
+
+func NewProtobufJSONToText(protoFile, messageName string) (converter streamconv.Converter, err error) {
+	files, err := parser.ParseFiles(protoFile)
+	if err != nil {
+		return
+	}
+
+	descriptor := files[0].FindMessage(messageName)
+	message := dynamic.NewMessage(descriptor)
+
+	converter = &jsonToText{message}
+	return
+}
+
+type textToJSON struct {
+	message    *dynamic.Message
+	textBuffer *bytes.Buffer
+}
+
+func (c *textToJSON) Convert(src io.Reader) (dst io.Reader, err error) {
+	c.textBuffer.Reset()
+	_, err = c.textBuffer.ReadFrom(src)
+	if err != nil {
+		return
+	}
+
+	err = proto.UnmarshalText(c.textBuffer.String(), c.message)
+	if err != nil {
+		return
+	}
+
+	pr, pw := io.Pipe()
+
+	go func() {
+		err = marshaller.Marshal(pw, c.message)
+		pw.CloseWithError(err)
+	}()
+
+	return pr, nil
+}
+
+func NewProtobufTextToJSON(protoFile, messageName string) (converter streamconv.Converter, err error) {
+	files, err := parser.ParseFiles(protoFile)
+	if err != nil {
+		return
+	}
+
+	descriptor := files[0].FindMessage(messageName)
+	message := dynamic.NewMessage(descriptor)
+
+	converter = &textToJSON{message, &bytes.Buffer{}}
 	return
 }
